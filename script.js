6f902568c8ca835da946155b98585a14b6b4b68a
Bradley Gosiengfiao <baseballbrad12@gmail.com>
2020-10-10 19:37:32 -0700

    Initializing my working files.

---
+++ script.js
@@ -0,0 +1,17 @@
+function InitiateQuiz(){
+
+    var time = 100;
+    
+    document.getElementsByTagName("h4").innerHTML = countdown(time);
+
+    var ArrayQues = [0,1];
+
+    var Question = "";
+    var MultChoice = "";
+    
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+}
